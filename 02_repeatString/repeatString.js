197e7ad5886d22e16580642fd0c41b96187a5675
tarikata777 <125801533+tarikata777@users.noreply.github.com>
2023-12-23 13:28:39 +0200

    02-repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let newWord = "";
+    if (num > 0){
+        for (let i = 0; i < num; i++){
+            newWord += str;
+        }
+        return newWord;
+    }
+    else if (num < 0){
+        return "ERROR";
+    }
+    else{
+        return "";
+    }
 };
-
 // Do not edit below this line
 module.exports = repeatString;
+``
