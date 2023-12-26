b4f54b2995960dd52aca21a0447e0419ac0d7d88
tarikata777 <125801533+tarikata777@users.noreply.github.com>
2023-12-26 14:37:43 +0200

    05-sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,26 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+   let sum = 0;
+   if ((start >= 0 && Number.isInteger(start)) && 
+   (end >= 0 && Number.isInteger(end))){
+        if (start > end){
+            let revEnd = start;
+            let revStart = end;
 
+            for (let i = 0; i < revEnd +1; i++){
+            sum += i;
+            }
+            return sum;
+        }
+
+        for (let i = 0; i < end +1; i++){
+            sum += i;
+        }
+        return sum;
+   }
+   else {
+    return "ERROR";
+   }
 };
-
+    console.log(sumAll(123,1));
 // Do not edit below this line
 module.exports = sumAll;
