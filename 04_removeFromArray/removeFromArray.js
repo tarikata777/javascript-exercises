2462e24388a70d19d12ac370c5980c711076f3c9
tarikata777 <125801533+tarikata777@users.noreply.github.com>
2023-12-26 11:53:26 +0200

    04-removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array1, ...args) {
+    args.forEach((arg) => {
+        const index = array1.indexOf(arg);
+        if (index > -1){
+            array1.splice(index,1);
+        }
+    });
+    return array1;
 };
 
 // Do not edit below this line
